f1a26f8e0d709e4fb9007fbad06a18f87e15fac5
GitHub git update Action <action@github.com>
2025-09-01 02:01:33 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_Z31.h
+++ generated/controllers/generated/rusefi_generated_Z31.h
@@ -1880,7 +1880,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.08.31.Z31.2239560323"
+#define TS_SIGNATURE "rusEFI main.2025.09.01.Z31.2239560323"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
